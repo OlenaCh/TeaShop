47ae304d83dcdf22350985a7b8de693bb7a6ef43
OlenaCh <olena.chernilevska@gmail.com>
2016-07-18 15:38:57 +0300

    Merge branch 'master' of https://github.com/OlenaCh/TeaShop
    Conflicts:
    	.idea/workspace.xml
    	app/controllers/users_controller.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718114027) do
+ActiveRecord::Schema.define(version: 20160718114332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20160718114027) do
     t.datetime "activated_at"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "zip_code"
+    t.string   "city"
+    t.string   "address"
     t.string   "confirm_token"
   end
 
