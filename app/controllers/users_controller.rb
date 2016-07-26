efb423e8103ba4101a4c560fd3fef4c196726b1d
OlenaCh <olena.chernilevska@gmail.com>
2016-07-26 16:04:19 +0300

    Devise - new methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,65 +1,48 @@
 class UsersController < ApplicationController
-#  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, 
-#                                        :following, :followers]
-#  before_action :correct_user,   only: [:edit, :update]
-#  before_action :admin_user,     only: :destroy
-  
-  def show
-    @user = User.find(params[:id])
-  end
-  
-#  def index
-#    @users = User.paginate(page: params[:page])
-#  end
+  before_action :authenticate_user!
+  #
+  # before_action :set_user, only: [:show, :edit, :update] # probably want to keep using this
 
-  def new
-    @user = User.new
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
   end
-  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_url
-    else
-      flash[:error] = "Ooooppss, something went wrong!"
-      render 'new'
+
+  # # GET /users/1
+  # # GET /users/1.json
+  def show
+
+  end
+
+  # GET /users/1/edit
+  def edit
+
+  end
+
+  # # PATCH/PUT /users/1
+  # # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
-
-#  def edit
-#   @user = User.find(params[:id])
-# end
-  
-#  def update
-#    @user = User.find(params[:id])
-#    if @user.update_attributes(user_params)
-#      flash[:success] = "Profile updated"
-#      redirect_to @user
-#    else
-#      render 'edit'
-#    end
-#  end
-  
-  # def destroy
-  #   User.find(params[:id]).destroy
-  #   flash[:success] = "User deleted"
-  #   redirect_to users_url
-  # end
 
   private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :zip_code, :city, :address, :email, :password)
-    end
-    
-  #   # Confirms the correct user.
-  #   def correct_user
-  #     @user = User.find(params[:id])
-  #     redirect_to(root_url) unless @user == current_user
-  #   end
-    
-    # # Confirms an admin user.
-    # def admin_user
-    #   redirect_to(root_url) unless current_user.admin?
-    # end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:role, :user_name)
+  end
+
 end
