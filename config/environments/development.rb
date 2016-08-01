77b3cf69cd31ec05363d9583741e2b0a66ddad06
OlenaCh <olena.chernilevska@gmail.com>
2016-08-01 12:21:09 +0300

    Registration controller - rspec (final)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
+
   host = 'localhost:3000'                                          #need to change
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
--- spec/controllers/api/v1/users/registrations_controller_spec.rb
+++ spec/controllers/api/v1/users/registrations_controller_spec.rb
@@ -26,25 +26,50 @@ RSpec.describe Api::V1::Users::RegistrationsController, type: :controller do
       end
     end
 
-    context 'with invalid params: already used or missing email' do
-      it 'does not create a new user' do
-        expect { post :create, user_params.merge({email: user.email}) }.to change(User, :count).by(0)
+    context 'with invalid params' do
+      context 'with duplicated email' do
+        it 'does not create a new user' do
+          expect { post :create, user_params.merge({email: user.email}) }.to change(User, :count).by(0)
+        end
+
+        it 'responds with HTTP status 422' do
+          post :create, user_params.merge({email: user.email})
+          expect(response.status).to eq 422
+        end
       end
 
-      # it 'responds with HTTP status 403' do
-      #   post :create, user_params.merge({email: ''})
-      #   expect(response.status).to eq 403
-      # end
-    end
+      context 'with missing email' do
+        it 'does not create a new user' do
+          expect { post :create, user_params.merge({email: ''}) }.to change(User, :count).by(0)
+        end
 
-    # it 'redirects to user\' page if user is saved' do
-    #   allow(@user).to receive(:save).and_return(true)
-    #   post :create, "user" => { name: 'Iryna Homlyak', zip_code: '79005', city: 'Lviv',
-    #                             address: 'Masaryka str., 3', email: 'irina_hom@ukr.net', password: '800000' }
-    #   expect(response).to redirect_to(root_url)
-    # end
+        it 'responds with HTTP status 422' do
+          post :create, user_params.merge({email: ''})
+          expect(response.status).to eq 422
+        end
+      end
+
+      context 'with missing password' do
+        it 'does not create a new user' do
+          expect { post :create, user_params.merge({password: ''}) }.to change(User, :count).by(0)
+        end
+
+        it 'responds with HTTP status 422' do
+          post :create, user_params.merge({password: ''})
+          expect(response.status).to eq 422
+        end
+      end
+    end
   end
 end
+
+
+# it 'redirects to user\' page if user is saved' do
+#   allow(@user).to receive(:save).and_return(true)
+#   post :create, "user" => { name: 'Iryna Homlyak', zip_code: '79005', city: 'Lviv',
+#                             address: 'Masaryka str., 3', email: 'irina_hom@ukr.net', password: '800000' }
+#   expect(response).to redirect_to(root_url)
+# end
 
 # RSpec.describe UsersController, type: :controller do
 # #   describe 'GET show' do
