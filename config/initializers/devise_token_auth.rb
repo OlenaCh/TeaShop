bcab82648f4648143ba083b255120bf0db94e4a7
OlenaCh <olena.chernilevska@gmail.com>
2016-08-01 18:54:59 +0300

    Sessions + passwords

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,5 +1,4 @@
 DeviseTokenAuth.setup do |config|
   config.remove_tokens_after_password_reset = true
   config.default_confirm_success_url = 'www.google.com'
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   mount_devise_token_auth_for 'User', at: 'api/v1',
       controllers: {
-      registrations:  'api/v1/users/registrations',
-      # sessions:       'api/v1/users/sessions',
-      passwords:      'api/v1/users/passwords'
+      registrations:  'api/v1/users/registrations'
   }
 
   root to: "application#index"
