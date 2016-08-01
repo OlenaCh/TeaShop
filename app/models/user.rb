ee2aa7228bca2c4480b56a82e1fcf7df5e551af7
OlenaCh <olena.chernilevska@gmail.com>
2016-08-01 15:09:38 +0300

    User model - rspec (final 1.0)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,28 @@
 class User < ActiveRecord::Base
-  # Include default devise modules.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :omniauthable
 
   include DeviseTokenAuth::Concerns::User
 
-  validates :name, presence: true, length: { in: 2..50 }
-  # validates :zip_code, presence: true, length: { in: 2..50 }
-  # validates :city, presence: true, length: { in: 2..50 }
-  # validates :address, presence: true, length: { in: 2..50 }
+  before_save :downcase_city, :downcase_address
+
+  validates :name, presence: true, length: { in: 2..40 }
+  validates :zip_code, presence: true, length: { in: 4..11 }
+
+  VALID_CITY_REGEX = /\A[a-zA-Z0-9\-\s]+\z/i
+  validates :city, presence: true, length: { in: 2..20 },
+            format: { with: VALID_CITY_REGEX }
+
+  validates :address, presence: true, length: { in: 2..40 }
+
+  private
+
+  def downcase_city
+    self.city = city.downcase
+  end
+
+  def downcase_address
+    self.address = address.downcase
+  end
 end
