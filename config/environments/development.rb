179b6b5b8edd5f14603aa090064d1908a015603c
OlenaCh <olena.chernilevska@gmail.com>
2016-08-02 18:01:15 +0300

    Deployment 7.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,25 +12,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      domain: 'gmail.com',
-      authentication: 'plain',
-      enable_starttls_auto: true,
-      user_name: Figaro.env.gmail_username,
-      password: Figaro.env.gmail_password,
-      openssl_verify_mode: 'none'
-  }
-
-  # config.action_mailer.default_url_options = { host: ENV["SMTP_DOMAIN"] }
-
-  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener
   host = 'mighty-sands-49511.herokuapp.com'                                          #need to change
-
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,28 +1,32 @@
 Rails.application.configure do
 
   config.cache_classes = true
-
   config.eager_load = true
-
   config.consider_all_requests_local       = false
-
   config.action_controller.perform_caching = true
 
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   config.assets.js_compressor = :uglifier
-
   config.assets.compile = false
-
   config.assets.digest = true
 
   config.log_level = :debug
-
   config.i18n.fallbacks = true
-
   config.active_support.deprecation = :notify
 
   config.log_formatter = ::Logger::Formatter.new
-
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      authentication: 'plain',
+      enable_starttls_auto: true,
+      user_name: ENV['gmail_username'],
+      password: ENV['gmail_password']
+  }
 end
