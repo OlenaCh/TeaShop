8295fa0c4a0ba7e7beadedd5f537e3dd7468b895
OlenaCh <olena.chernilevska@gmail.com>
2016-08-05 11:43:38 +0300

    Division between admin and users

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,9 +1,20 @@
 class Api::V1::Users::SessionsController < DeviseTokenAuth::SessionsController
   def create
     super
+  end
+
+  private
+
+  def render_create_success
     @user = User.find_by_email(resource_params[:email])
-    # if !!@user
-    #   admin_path if @user.role == 'user'
-    # end
+    if @user.role == 'admin'
+      # render json: {
+      #     action: 'admin#show'
+      # }
+    else
+      render json: {
+        data: resource_data(resource_json: @resource.token_validation_response)
+      }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   mount_devise_token_auth_for 'User', at: 'api/v1',
       controllers: {
-      registrations:  'api/v1/users/registrations'
+      registrations:  'api/v1/users/registrations',
+      sessions:       'api/v1/users/sessions'
   }
 
   root to: "application#index"
