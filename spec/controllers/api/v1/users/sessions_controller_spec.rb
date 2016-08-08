2dd592fdfce34e8c7907cbb38ed42447bddb74f3
OlenaCh <olena.chernilevska@gmail.com>
2016-08-08 17:46:14 +0300

    Rspec before second demo 1.0

---
+++ spec/controllers/api/v1/users/sessions_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::Users::SessionsController, type: :controller do
+  describe '#create' do
+    # it 'creates a user' do
+    #   expect_any_instance_of(User).to receive(:create).
+    #       with({name: 'Sideshow Bob'}.with_indifferent_access)
+    #   post :create, user:
+    #       { first_name: 'Sideshow', last_name: 'Bob', name: 'Sideshow Bob' }
+    # end
+  end
+
+  describe '#render_create_success' do
+    # it 'creates a user' do
+    #   expect_any_instance_of(User).to receive(:create).
+    #       with({name: 'Sideshow Bob'}.with_indifferent_access)
+    #   post :create, user:
+    #       { first_name: 'Sideshow', last_name: 'Bob', name: 'Sideshow Bob' }
+    # end
+  end
+end
---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, type: :controller do
+  describe 'strong parameters' do
+    # it 'creates a user' do
+    #   expect_any_instance_of(User).to receive(:create).
+    #       with({name: 'Sideshow Bob'}.with_indifferent_access)
+    #   post :create, user:
+    #       { first_name: 'Sideshow', last_name: 'Bob', name: 'Sideshow Bob' }
+    # end
+  end
+end
