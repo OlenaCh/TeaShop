b68dd6c7810bb2207d70a2cb7f59fae0a48fa7c6
OlenaCh <olena.chernilevska@gmail.com>
2016-08-09 12:23:07 +0300

    Rspec before second demo 3.0

--- spec/controllers/api/v1/users/sessions_controller_spec.rb
+++ spec/controllers/api/v1/users/sessions_controller_spec.rb
@@ -14,43 +14,54 @@ RSpec.describe Api::V1::Users::SessionsController, type: :controller do
     context 'with valid attributes' do
       it 'logs in user' do
         post :create, { email: user.email, password: user.password }
+        allow(request.env['warden']).to receive(:authenticate!).and_return(user)
         expect(subject.current_user).to eq user
       end
     end
 
-    # context 'with invalid attributes' do
-    #   it 'not logged in user' do
-    #     post :create, { email: '', password: user.password }
-    #     expect(subject.current_user).to eq(nil)
-    #     expect(JSON.parse(response.body)["errors"]).to eq sign_in_error
-    #   end
-    #
-    #   it 'not logged in user' do
-    #     post :create, { email: user.email, password: 'sdsfsfss' }
-    #     expect(JSON.parse(response.body)["errors"]).to eq sign_in_error
-    #   end
-    #
-    #   it 'not logged in user' do
-    #     post :create, { email: '', password: '' }
-    #     expect(response.status).to eq(401)
-    #   end
-    # end
+    context 'with unconfirmed email' do
+      it 'does not log in' do
+        post :create, { email: user.email, password: user.password }
+        allow(request.env['warden']).to receive(:authenticate!).and_return(nil)
+        expect(JSON.parse(response.body)).to eq "success" => false, "errors" => [I18n.t("devise_token_auth.sessions.not_confirmed")]
+      end
+    end
+
+    context 'with invalid attributes' do
+      before(:each) do
+        user.confirm
+      end
+
+      it 'does not log in without email' do
+        post :create, { email: '', password: user.password }
+        expect(subject.current_user).to eq(nil)
+        expect(JSON.parse(response.body)).to eq "errors" => [I18n.t("devise_token_auth.sessions.bad_credentials")]
+      end
+
+      it 'does not log in without password' do
+        post :create, { email: user.email, password: '' }
+        expect(JSON.parse(response.body)).to eq "errors" => [I18n.t("devise_token_auth.sessions.bad_credentials")]
+      end
+
+      it 'does not log in with wrong password' do
+        post :create, { email: user.email, password: 'aaaaraaaa' }
+        expect(JSON.parse(response.body)).to eq "errors" => [I18n.t("devise_token_auth.sessions.bad_credentials")]
+      end
+    end
   end
 
-  # describe 'DELETE #destory' do
-  #   before(:each) do
-  #     post :create, { email: user.email, password: user.password }
-  #
-  #     @header = { "access-token" => response.header["access-token"],
-  #                 "client" => response.header["client"],
-  #                 "uid" => response.header["uid"] }
-  #   end
-  #
-  #   context 'with existing user' do
-  #     it 'signs out user' do
-  #       delete :destroy, {}, @header
-  #       #expect(response.status).to eq(200)
-  #     end
-  #   end
-  # end
+  describe 'DELETE #destory' do
+    before(:each) do
+      post :create, { email: user.email, password: user.password }
+      @header = { "access-token" => response.header["access-token"],
+                  "client" => response.header["client"],
+                  "uid" => response.header["uid"] }
+    end
+
+    context 'with existing user' do
+      it 'logs out user' do
+        delete :destroy, {}, @header
+      end
+    end
+  end
 end
