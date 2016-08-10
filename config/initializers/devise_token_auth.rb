8065fdd3e23e872d393bd1f0cb5f0e60bb726d3e
OlenaCh <olena.chernilevska@gmail.com>
2016-08-10 12:36:18 +0300

    Orders: empty specs, controller & model

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,4 +1,4 @@
 DeviseTokenAuth.setup do |config|
   config.remove_tokens_after_password_reset = true
-  config.default_confirm_success_url = 'agile-wave-90174.herokuapp.com'
+  config.default_confirm_success_url = 'tea-shop.herokuapp.com'
 end
