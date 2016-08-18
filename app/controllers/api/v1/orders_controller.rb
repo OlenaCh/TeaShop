1de73851d81208a4626bded40ee52a97980fe2e2
OlenaCh <olena.chernilevska@gmail.com>
2016-08-18 18:43:52 +0300

    Orders 7.0

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -12,23 +12,18 @@ class Api::V1::OrdersController < ApplicationController
 
   def create
     subtotal = 0
-    total = 0
     current_user = User.find_by_email(params[:email])
     @order = current_user.orders.create(shipment: order_params[:shipment].to_i)
-    products = order_params[:products]
-    products.each do |item|
-      details = item.last
-      product_id = details[:id].to_i
-      product_amount = details[:amount].to_i
-      @order.orders_products.create(product_id: product_id, amount: product_amount)
-      subtotal = subtotal + product_amount * Product.find_by_id(product_id).price
-      total = order_params[:shipment].to_i + subtotal
+    order_params[:products].each do |item|
+      @order.orders_products.create(product_id: item.last[:id].to_i, amount: item.last[:amount].to_i)
+      subtotal = subtotal + item.last[:amount].to_i * Product.find_by_id(item.last[:id].to_i).price
     end
-    @order.update(subtotal: subtotal, grand_total: total)
+    @order.update(subtotal: subtotal, grand_total: order_params[:shipment].to_i + subtotal)
     if @order.save
       render status: 200, json: @order
     else
-      render status: 400, json: @order.errors
+      # @order.orders_products.destroy
+      render status: 400, json: @order.errors #{ errors: ["No order created!"] }
     end
   end
 
@@ -51,18 +46,6 @@ class Api::V1::OrdersController < ApplicationController
         errors: ["Users only."]
     }, status: 401
   end
-
-  # def render_creation_error
-  #   render json: {
-  #       errors: ["No order created!"]
-  #   }, status: 400
-  # end
-  #
-  # def render_parameters_error
-  #   render json: {
-  #       errors: ["Unprocessable parameters!"]
-  #   }, status: 422
-  # end
   #
   # def just_do_sth_with product
   #   @order = create_order product.price
@@ -101,22 +84,6 @@ class Api::V1::OrdersController < ApplicationController
   #     orders_product = order.orders_products.create(amount: order_params[:amount].to_i, product_id: product)
   #   end
   #   orders_product
-  # end
-  #
-  # def count_value price
-  #   order_params[:amount].to_i * price
-  # end
-  #
-  # def find_product
-  #   Product.find_by_id(order_params[:product_id])
-  # end
-  #
-  # def count_subtotal(subtotal, price)
-  #   subtotal + order_params[:amount].to_i * price
-  # end
-  #
-  # def count_grand_total(grand_total, price)
-  #   grand_total + order_params[:amount].to_i * price
   # end
 
   def order_params
--- app/models/orders_product.rb
+++ app/models/orders_product.rb
@@ -1,5 +1,5 @@
 class OrdersProduct < ActiveRecord::Base
-  belongs_to :product, dependent: :destroy
+  belongs_to :product
   belongs_to :order, dependent: :destroy
 
   validates :amount, :numericality => { :greater_than_or_equal_to => 0 }
