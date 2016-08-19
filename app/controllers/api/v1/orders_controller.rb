9bdd8f7c63aa213d31977142bb0769e9faaf85ce
OlenaCh <olena.chernilevska@gmail.com>
2016-08-19 17:11:07 +0300

    Orders 9.0

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -3,32 +3,65 @@ class Api::V1::OrdersController < ApplicationController
   # before_action :authenticate_user!, only: [:index]
 
   def index
-    #
-  end
-
-  def show
-    # @data = Order.find(params[:id])
+    @list = Array.new
+    # @orders = Order.all.orders_products
+    @orders = Order.all
+    @orders.each do |order|
+      @list << order
+      order.orders_products.each do |order_product|
+        item = Array.new
+        item << order_product << Product.find_by_id(order_product.product_id)
+      end
+    end
+    # @orders.each do |order|
+    #   items = order.orders_products
+    #   items.each do |item|
+    #     id = order.order_id
+    #     product = Product.find_by_id(item.product_id)
+    #     title = product.title
+    #     price = order.orders_products.amount * product.price
+    #     @list << Hash.new(:id => id, :title => title, :price => price)
+    #   end
+    # end
+    render json: @list
   end
 
   def create
     subtotal = 0
     current_user = User.find_by_email(params[:email])
-    @order = current_user.orders.create(shipment: order_params[:shipment].to_i)
-    order_params[:products].each do |item|
+    @order = current_user.orders.create(shipment: create_params[:shipment].to_i)
+    create_params[:products].each do |item|
       @object = @order.orders_products.create(product_id: item.last[:id].to_i, amount: item.last[:amount].to_i)
       subtotal = subtotal + item.last[:amount].to_i * Product.find_by_id(item.last[:id].to_i).price
     end
-    @order.update(subtotal: subtotal, grand_total: order_params[:shipment].to_i + subtotal)
+    @order.update(subtotal: subtotal, grand_total: create_params[:shipment].to_i + subtotal)
     if @order.save
       render status: 200, json: @order
     else
       @order.destroy
-      render status: 400, json: { errors: ["No order created!"] }
+      render status: 400, json: { errors: ['No order created!'] }
     end
   end
 
   def update
-    #
+    @order = Order.find_by_id(params[:id])
+    if @order
+      @order.update(status: update_params[:status])
+      render status: 200, json: @order
+    else
+      render_not_found 'Object not found'
+    end
+  end
+
+  def destroy
+    @order = Order.find_by_id(params[:id])
+    if @order
+      @order.destroy
+      # redirect_to api_v1_orders_path, format: :json
+      render status: 200, json: { notice: ['Success'] }
+    else
+      render_not_found 'Object not found'
+    end
   end
 
   private
@@ -43,50 +76,15 @@ class Api::V1::OrdersController < ApplicationController
 
   def render_admin_authorization_error
     render json: {
-        errors: ["Users only."]
+        errors: ['Users only.']
     }, status: 401
   end
-  #
-  # def just_do_sth_with product
-  #   @order = create_order product.price
-  #   if !(@order.save)
-  #     render_creation_error
-  #   else
-  #     @orders_product = create_element_of_order_list(@order, product.id)
-  #     if !(@orders_product.save)
-  #       @order.destroy
-  #       render_creation_error
-  #     else
-  #       render status: 200, json: [@order, @orders_product]
-  #     end
-  #   end
-  # end
-  #
-  # def create_order price
-  #   user = User.find_by_email(params[:email])
-  #   # user = current_user
-  #   if user.current_order == 0
-  #     order = user.orders.create(subtotal: count_value(price), grand_total: count_value(price))
-  #     user.update(current_order: order.id)
-  #   else
-  #     order = user.orders.find(user.current_order)
-  #     order.update(subtotal: count_subtotal(order.subtotal, price),
-  #                  grand_total: count_grand_total(order.grand_total, price))
-  #   end
-  #   order
-  # end
-  #
-  # def create_element_of_order_list(order, product)
-  #   orders_product = order.orders_products.find_by_product_id(product)
-  #   if orders_product
-  #     orders_product.update(amount: orders_product.amount + order_params[:amount].to_i)
-  #   else
-  #     orders_product = order.orders_products.create(amount: order_params[:amount].to_i, product_id: product)
-  #   end
-  #   orders_product
-  # end
 
-  def order_params
+  def create_params
     params.permit(:shipment, products: [:id, :amount])
+  end
+
+  def update_params
+    params.permit(:status)
   end
 end
